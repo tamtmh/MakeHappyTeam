d10820bbac1c567e9a8bc014b30e502a5a068072
Tampv <tampv@tmh-techlab.vn>
2019-09-09 16:32:30 +0700

    import database

---
+++ sql-data/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "myuser";
+$password = "password";
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully\n";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage() . "\n";
+    die(1);
+}
